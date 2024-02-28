b3bd4b1a2c21c3f8eb719bcd303dc2e37ab71487
float-me <main.float@proton.me>
2024-02-28 16:51:46 +0900

    줄바꿈 처리

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const input_box = document.getElementById("inputBox");
 input_box.focus();
 const tag_editor = document.getElementById("tagEditor");
-// const tag_container = tag_editor.getElementsByClassName("tag-container");
+const tag_container = tag_editor.firstElementChild;
 
 input_box.addEventListener("keydown", (event) => {
     if (event.isComposing) return;
@@ -11,7 +11,7 @@ input_box.addEventListener("keydown", (event) => {
         let newContent = document.createTextNode(input_box.textContent);
         newTag.appendChild(newContent)
 
-        tag_editor.insertBefore(newTag, input_box)
+        tag_container.insertBefore(newTag, input_box)
         input_box.textContent = ""
         event.preventDefault()
     }
