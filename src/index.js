712369d2fc843eea46b2d42fbde4a74c42243ef0
float-me <main.float@proton.me>
2024-02-28 18:06:09 +0900

    모바일용 테스트2

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ const tag_container = tag_editor.firstElementChild;
 
 input_box.addEventListener("keydown", (event) => {
     if (event.isComposing) return;
-    alert(event.code);
-    if (event.code === 'Space') {
+    if (event.key === ' ') {
         let newTag = document.createElement("span");
         newTag.classList.add("char-btn");
         let newContent = document.createTextNode(input_box.textContent);
@@ -15,7 +14,7 @@ input_box.addEventListener("keydown", (event) => {
         tag_container.insertBefore(newTag, input_box)
         input_box.textContent = ""
         event.preventDefault()
-    } else if (event.code === 'Backspace') {
+    } else if (event.key === 'Backspace') {
         if (input_box.textContent === "" && tag_container.childElementCount >= 2) {
             let tag_to_erase = Array.from(document.querySelectorAll("span:nth-last-of-type(2)")).pop();
             tag_to_erase.remove();
